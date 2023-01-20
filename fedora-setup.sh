57fd0491e8fa5fa7fc9a72fa9315f2334b53cf15
Fabeuss <40515597+Fabeuss@users.noreply.github.com>
2023-01-20 14:17:21 +0000

    Update fedora-setup.sh

--- fedora-setup.sh
+++ fedora-setup.sh
@@ -7,9 +7,6 @@ BACKTITLE="Fedora Setup Util - By Osiris - https://lsass.co.uk"
 TITLE="Please Make a selection"
 MENU="Please Choose one of the following options:"
 
-#Other variables
-OH_MY_ZSH_URL="https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh"
-
 #Check to see if Dialog is installed, if not install it - Thanks Kinkz_nl
 if [ $(rpm -q dialog 2>/dev/null | grep -c "is not installed") -eq 1 ]; then
 sudo dnf install -y dialog
