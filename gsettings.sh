dff9f01f588c77f14f992f46b06dcc1a4708db9e
Fabeuss <40515597+Fabeuss@users.noreply.github.com>
2023-02-24 11:10:24 +0100

    Update gsettings.sh

--- gsettings.sh
+++ gsettings.sh
@@ -11,9 +11,12 @@ gsettings set org.gnome.desktop.interface clock-show-seconds false
 gsettings set org.gnome.desktop.interface clock-show-weekday true
 gsettings set org.gnome.desktop.interface cursor-theme 'capitaine-cursors'
 gsettings set org.gnome.desktop.interface icon-theme 'Papirus-Dark'
-
-# Enable window buttons
 gsettings set org.gnome.desktop.wm.preferences button-layout ":minimize,maximize,close"
+gsettings set org.gnome.desktop.peripherals.mouse accel-profile 'adaptive'
+gsettings set org.gnome.desktop.sound allow-volume-above-100-percent true
+gsettings set org.gnome.desktop.calendar show-weekdate true
+gsettings set org.gnome.desktop.wm.preferences resize-with-right-button true
+gsettings set org.gnome.shell.overrides workspaces-only-on-primary false
 
 # Set new windows centered
 gsettings set org.gnome.mutter center-new-windows true
