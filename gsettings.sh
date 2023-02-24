3bddd7e52c938f74669b8cba854ef77a7893c29d
Fabeuss <40515597+Fabeuss@users.noreply.github.com>
2023-02-24 19:14:07 +0100

    Update gsettings.sh

--- gsettings.sh
+++ gsettings.sh
@@ -4,11 +4,13 @@ gsettings set org.gnome.desktop.interface document-font-name 'Noto Sans 11'
 gsettings set org.gnome.desktop.interface font-name 'Noto Sans 11'
 gsettings set org.gnome.desktop.wm.preferences titlebar-font 'Noto Sans 11'
 
-# Other Settings
+# Clock Settings
 gsettings set org.gnome.desktop.interface clock-format '24h'
 gsettings set org.gnome.desktop.interface clock-show-date true
 gsettings set org.gnome.desktop.interface clock-show-seconds false
 gsettings set org.gnome.desktop.interface clock-show-weekday true
+
+# Other Settings
 gsettings set org.gnome.desktop.interface cursor-theme 'capitaine-cursors'
 gsettings set org.gnome.desktop.interface icon-theme 'Papirus-Dark'
 gsettings set org.gnome.desktop.wm.preferences button-layout ":minimize,maximize,close"
@@ -17,14 +19,17 @@ gsettings set org.gnome.desktop.sound allow-volume-above-100-percent true
 gsettings set org.gnome.desktop.calendar show-weekdate true
 gsettings set org.gnome.desktop.wm.preferences resize-with-right-button true
 gsettings set org.gnome.shell.overrides workspaces-only-on-primary false
+gsettings set org.gtk.Settings.FileChooser sort-directories-first true
+
+# Dash-to-dock Settings
 gsettings set org.gnome.shell.extensions.dash-to-dock transparency-mode 'FIXED'
 gsettings set org.gnome.shell.extensions.dash-to-dock show-apps-at-top true
 gsettings set org.gnome.shell.extensions.dash-to-dock intellihide-mode 'ALL_WINDOWS'
-gsettings set org.gtk.Settings.FileChooser sort-directories-first true
+
+# Nautilus Settings
 gsettings set org.gnome.nautilus.preferences show-hidden-files true
 gsettings set org.gnome.nautilus.preferences sort-directories-first true
 gsettings set org.gnome.nautilus.preferences default-folder-viewer 'list-view'
-
 
 # Set new windows centered
 gsettings set org.gnome.mutter center-new-windows true
