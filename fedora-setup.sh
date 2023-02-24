c6fda0cfcc8cb487e7fcff1523b2d18c544d8668
Fabeuss <40515597+Fabeuss@users.noreply.github.com>
2023-02-24 08:24:48 +0100

    Update fedora-setup.sh

--- fedora-setup.sh
+++ fedora-setup.sh
@@ -41,25 +41,25 @@ while [ "$CHOICE -ne 4" ]; do
             sudo dnf groupupdate -y core
             sudo dnf install -y rpmfusion-free-release-tainted
 	    sudo dnf install -y rpmfusion-nonfree-release-tainted
-            sudo dnf install -y dnf-plugins-core
+            sudo dnf install -y dnf-plugins-core; exec $SHELL;
             notify-send "RPM Fusion Enabled" --expire-time=10
            ;;
         2)  echo "Updating Firmware"
             sudo fwupdmgr get-devices 
             sudo fwupdmgr refresh --force 
             sudo fwupdmgr get-updates 
-            sudo fwupdmgr update
+            sudo fwupdmgr update; exec $SHELL;
            ;;
         3)  echo "Speeding Up DNF"
             echo 'fastestmirror=1' | sudo tee -a /etc/dnf/dnf.conf
             echo 'max_parallel_downloads=10' | sudo tee -a /etc/dnf/dnf.conf
-            echo 'deltarpm=true' | sudo tee -a /etc/dnf/dnf.conf
+            echo 'deltarpm=true' | sudo tee -a /etc/dnf/dnf.conf; exec $SHELL;
             notify-send "Your DNF config has now been amended" --expire-time=10
            ;;
         4)  echo "Enabling Flatpak"
             flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
             flatpak update
-            source 'flatpak-install.sh'
+            source 'flatpak-install.sh'; exec $SHELL;
             notify-send "Flatpak has now been enabled" --expire-time=10
            ;;
         5)  echo "Installing Software"
@@ -70,13 +70,13 @@ while [ "$CHOICE -ne 4" ]; do
             sudo dnf copr enable aleasto/waydroid -y
 	    sudo dnf update -y
             sudo dnf install waydroid
-	    sudo systemctl enable --now waydroid-container
+	    sudo systemctl enable --now waydroid-container; exec $SHELL;
             notify-send "Waydroid has been installed" --expire-time=10
            ;;
         7)  echo "Installing Librewolf"
             sudo dnf config-manager --add-repo https://rpm.librewolf.net/librewolf-repo.repo -y
 	    sudo dnf update -y
-	    sudo dnf install librewolf
+	    sudo dnf install librewolf; exec $SHELL;
             notify-send "Librewolf has been installed" --expire-time=10
            ;;
         8)  echo "Installing Extra Fonts"
@@ -86,14 +86,14 @@ while [ "$CHOICE -ne 4" ]; do
             sudo -s dnf install -y fontconfig-font-replacements
             sudo -s dnf install -y fontconfig-enhanced-defaults
 	    sudo dnf update -y
-	    sudo dnf install -y iosevka-term-fonts jetbrains-mono-fonts-all terminus-fonts terminus-fonts-console google-roboto google-noto-fonts-common mscore-fonts-all fira-code-fonts
+	    sudo dnf install -y iosevka-term-fonts jetbrains-mono-fonts-all terminus-fonts terminus-fonts-console google-roboto google-noto-fonts-common mscore-fonts-all fira-code-fonts; exec $SHELL;
             notify-send "All done" --expire-time=10
            ;;
         9)  echo "Installing media codecs"
             sudo dnf groupupdate -y sound-and-video
             sudo dnf install -y gstreamer1-plugins-{bad-\*,good-\*,ugly-\*,base} gstreamer1-libav --exclude=gstreamer1-plugins-bad-free-devel ffmpeg gstreamer-ffmpeg
             sudo dnf install -y lame\* --exclude=lame-devel
-            sudo dnf group upgrade -y --with-optional Multimedia
+            sudo dnf group upgrade -y --with-optional Multimedia; exec $SHELL;
            ;;
 	10)
           exit 0
