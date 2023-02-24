851d2eb33c47dc00a11cbf7295b1aa35cb51ce56
Fabeuss <40515597+Fabeuss@users.noreply.github.com>
2023-02-24 11:04:10 +0100

    Update gsettings.sh

--- gsettings.sh
+++ gsettings.sh
@@ -9,6 +9,8 @@ gsettings set org.gnome.desktop.interface clock-format '24h'
 gsettings set org.gnome.desktop.interface clock-show-date true
 gsettings set org.gnome.desktop.interface clock-show-seconds false
 gsettings set org.gnome.desktop.interface clock-show-weekday true
+gsettings set org.gnome.desktop.interface cursor-theme 'capitaine-cursors'
+gsettings set org.gnome.desktop.interface icon-theme 'Papirus-Dark'
 
 # Enable window buttons
 gsettings set org.gnome.desktop.wm.preferences button-layout ":minimize,maximize,close"
