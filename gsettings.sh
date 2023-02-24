1357aaa35952c1bc75f182e5d77e74d513ba4204
Fabeuss <40515597+Fabeuss@users.noreply.github.com>
2023-02-24 10:30:16 +0100

    Update gsettings.sh

--- gsettings.sh
+++ gsettings.sh
@@ -2,14 +2,13 @@
 #Font Settings
 gsettings set org.gnome.desktop.interface document-font-name 'Noto Sans 11'
 gsettings set org.gnome.desktop.interface font-name 'Noto Sans 11'
-gsettings set org.gnome.desktop.interface monospace-font-name 'JetBrains Mono 12'
 gsettings set org.gnome.desktop.wm.preferences titlebar-font 'Noto Sans 11'
 
 # Other Settings
 gsettings set org.gnome.desktop.interface clock-format '24h'
 gsettings set org.gnome.desktop.interface clock-show-date true
 gsettings set org.gnome.desktop.interface clock-show-seconds false
-gsettings set org.gnome.desktop.interface clock-show-weekday false
+gsettings set org.gnome.desktop.interface clock-show-weekday true
 
 # Enable window buttons
 gsettings set org.gnome.desktop.wm.preferences button-layout ":minimize,maximize,close"
